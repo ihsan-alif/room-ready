0510fa8ef93de2f6e09854cb0e22334a62641b3e
Bayu Saputra <Bayuarjuansyah32@gmail.com>
2024-01-08 13:33:22 +0700

    [FEATURE] add master branch

---
+++ src/main/java/app/roomready/roomready/booking/app/RoomReadyAppApplication.java
@@ -0,0 +1,13 @@
+package app.roomready.roomready.booking.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReadyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReadyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/roomready/roomready/booking/app/RoomReadyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.roomready.roomready.booking.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReadyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
