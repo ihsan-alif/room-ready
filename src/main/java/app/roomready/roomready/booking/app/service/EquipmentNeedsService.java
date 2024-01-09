7ad5e765ef5d50fc0ccca94f9ffbf1ac809a636f
maulanamasiqbal <maulanamasiqbal@gmail.com>
2024-01-09 10:21:28 +0700

    [FEATURE] add new feature api get all(search) equipment needs

--- src/main/java/app/roomready/roomready/booking/app/service/EquipmentNeedsService.java
+++ src/main/java/app/roomready/roomready/booking/app/service/EquipmentNeedsService.java
@@ -1,8 +1,10 @@
 package app.roomready.roomready.booking.app.service;
 
 import app.roomready.roomready.booking.app.dto.request.EquipmentRequest;
+import app.roomready.roomready.booking.app.dto.request.SearchEquipmentRequest;
 import app.roomready.roomready.booking.app.dto.response.EquipmentNeedsResponse;
 import app.roomready.roomready.booking.app.entity.EquipmentNeeds;
+import org.springframework.data.domain.Page;
 
 public interface EquipmentNeedsService {
 
@@ -15,4 +17,6 @@ public interface EquipmentNeedsService {
     EquipmentNeedsResponse update(EquipmentRequest request);
 
     void delete(String id);
+
+    Page<EquipmentNeedsResponse> search(SearchEquipmentRequest request);
 }
--- src/main/java/app/roomready/roomready/booking/app/service/impl/EquipmentNeedsImpl.java
+++ src/main/java/app/roomready/roomready/booking/app/service/impl/EquipmentNeedsImpl.java
@@ -1,17 +1,27 @@
 package app.roomready.roomready.booking.app.service.impl;
 
 import app.roomready.roomready.booking.app.dto.request.EquipmentRequest;
+import app.roomready.roomready.booking.app.dto.request.SearchEquipmentRequest;
 import app.roomready.roomready.booking.app.dto.response.EquipmentNeedsResponse;
 import app.roomready.roomready.booking.app.entity.EquipmentNeeds;
 import app.roomready.roomready.booking.app.repository.EquipmentNeedsRepository;
 import app.roomready.roomready.booking.app.service.EquipmentNeedsService;
 import app.roomready.roomready.booking.app.utils.ValidationUtils;
+import jakarta.persistence.criteria.Predicate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -88,6 +98,44 @@ public class EquipmentNeedsImpl implements EquipmentNeedsService {
 
         repository.delete(equipmentNeeds);
     }
+
+    @Override
+    public Page<EquipmentNeedsResponse> search(SearchEquipmentRequest request) {
+
+        Specification<EquipmentNeeds> specification = getEquipmentNeedsSpecification(request);
+
+        if (request.getPage() <= 0) request.setPage(1);
+
+        Pageable pageable = PageRequest.of(request.getPage() - 1, request.getSize());
+        Page<EquipmentNeeds> all = repository.findAll(specification, pageable);
+
+        List<EquipmentNeedsResponse> responses = new ArrayList<>();
+
+        for (EquipmentNeeds equipmentNeeds : all){
+            EquipmentNeedsResponse response = toEquipmentNeedsResponse(equipmentNeeds);
+
+            responses.add(response);
+        }
+
+        return new PageImpl<>(responses, pageable, all.getTotalElements());
+    }
+
+    private static Specification<EquipmentNeeds> getEquipmentNeedsSpecification(SearchEquipmentRequest request) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (Objects.nonNull(request.getName())){
+                predicates.add(criteriaBuilder.like(root.get("name"), "%"+ request.getName()+"%"));
+            }
+
+            if (Objects.nonNull(request.getQuantity())) {
+                predicates.add(criteriaBuilder.equal(root.get("quantity"), request.getQuantity()));
+            }
+
+            return query.where(predicates.toArray(new Predicate[]{})).getRestriction();
+        };
+    }
+
 
     private EquipmentNeedsResponse toEquipmentNeedsResponse(EquipmentNeeds equipmentNeeds){
         return EquipmentNeedsResponse.builder()
