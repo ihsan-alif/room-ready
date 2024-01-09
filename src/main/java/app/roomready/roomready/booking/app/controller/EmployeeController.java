c434a44b6dcee9847227d822d3baaf494c96e414
maulanamasiqbal <maulanamasiqbal@gmail.com>
2024-01-09 12:56:02 +0700

    [FEATURE] add new feature api get by id employee

---
+++ src/main/java/app/roomready/roomready/booking/app/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package app.roomready.roomready.booking.app.controller;
+
+import app.roomready.roomready.booking.app.dto.response.EmployeeResponse;
+import app.roomready.roomready.booking.app.dto.response.WebResponse;
+import app.roomready.roomready.booking.app.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employee")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService service;
+
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<WebResponse<EmployeeResponse>> getById(@PathVariable("employeeId") String id){
+        EmployeeResponse byId = service.getById(id);
+
+        WebResponse<EmployeeResponse> response = WebResponse.<EmployeeResponse>builder()
+                .status(HttpStatus.OK.getReasonPhrase())
+                .message("successfully get by id employee")
+                .data(byId)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
+}
--- src/main/java/app/roomready/roomready/booking/app/service/impl/EmployeeServiceImpl.java
+++ src/main/java/app/roomready/roomready/booking/app/service/impl/EmployeeServiceImpl.java
@@ -1,10 +1,13 @@
 package app.roomready.roomready.booking.app.service.impl;
 
+import app.roomready.roomready.booking.app.dto.response.EmployeeResponse;
 import app.roomready.roomready.booking.app.entity.Employee;
+import app.roomready.roomready.booking.app.entity.UserCredential;
 import app.roomready.roomready.booking.app.repository.EmployeeRepository;
 import app.roomready.roomready.booking.app.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,6 +20,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository repository;
 
+    private static final String ROLE = "ROLE_ADMIN";
+
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void create(Employee employee) {
@@ -28,9 +33,48 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
+    public EmployeeResponse getById(String id) {
+
+        Employee employee = repository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "not found")
+        );
+
+        UserCredential currentUser = (UserCredential) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserCredential userCredential = employee.getUserCredential();
+
+        if (!currentUser.getId().equals(userCredential.getId()) &&
+                currentUser.getAuthorities().stream().noneMatch(role -> role.getAuthority().equals(ROLE)))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Forbidden");
+
+        return toEmployeeResponse(employee);
+    }
+
+
+    @Override
     public Employee get(String id) {
-        return repository.findById(id).orElseThrow(
+        Employee employee = repository.findById(id).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found")
         );
+
+        UserCredential currentUser = (UserCredential) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserCredential userCredential = employee.getUserCredential();
+
+        if (!currentUser.getId().equals(userCredential.getId()) &&
+                currentUser.getAuthorities().stream().noneMatch(role -> role.getAuthority().equals(ROLE)))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Forbidden");
+
+        return employee;
+    }
+
+
+    private EmployeeResponse toEmployeeResponse(Employee employee){
+        return EmployeeResponse.builder()
+                .id(employee.getId())
+                .name(employee.getName())
+                .division(employee.getDivision())
+                .position(employee.getPosition())
+                .contactInfo(employee.getContactInfo())
+                .username(employee.getUserCredential().getUsername())
+                .build();
     }
 }
